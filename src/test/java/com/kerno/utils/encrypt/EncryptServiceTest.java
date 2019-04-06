78801ed1e6ea5747f2a6a5369e53913b8bfa5db3
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2019-04-06 12:38:29 -0500

    add test

--- src/test/java/com/kerno/utils/encrypt/EncryptServiceTest.java
+++ src/test/java/com/kerno/utils/encrypt/EncryptServiceTest.java
@@ -1,7 +1,13 @@
 package com.kerno.utils.encrypt;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EncryptServiceTest {
+    @Test
+    public void testGetMessage() {
+        assertEquals("Hello World!", "Hello World!");
+    }
 
 }
