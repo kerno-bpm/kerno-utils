cf7ef559ff21986423e8d5f4c6f50ad8ac918ac3
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2019-06-07 00:41:01 -0500

    Adding abstract dto.

---
+++ src/main/java/com/kerno/utils/dto/AbstractDto.java
@@ -0,0 +1,11 @@
+package com.kerno.utils.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class AbstractDto implements Serializable {
+    private Long id;
+    private boolean active;
+}
