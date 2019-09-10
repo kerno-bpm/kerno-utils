01d8e258b3fc1cdafd9427015c8bd64e55c2dcb0
Jorge Luis Malla <jmalla@ioet.com>
2019-09-10 16:33:18 -0500

    Adding exception.

---
+++ src/main/java/com/kerno/utils/exception/KernoConflictException.java
@@ -0,0 +1,11 @@
+package com.kerno.utils.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class KernoConflictException extends RuntimeException {
+    public KernoConflictException(String message) {
+        super(message);
+    }
+}
