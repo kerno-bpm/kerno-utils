d2810b7dc023a1cc4698e5c0d09ef1af29ef857a
Jorge Luis Malla <jmalla@ioet.com>
2019-09-10 17:45:28 -0500

    P13

--- src/main/java/com/kerno/utils/exception/KernoConflictException.java
+++ src/main/java/com/kerno/utils/exception/KernoConflictException.java
@@ -1,9 +1,5 @@
 package com.kerno.utils.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.CONFLICT)
 public class KernoConflictException extends RuntimeException {
     public KernoConflictException(String message) {
         super(message);
