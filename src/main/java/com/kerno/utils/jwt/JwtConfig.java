e634591699bf2352e5f8c3fa8fdab836eaa91bb1
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2019-09-11 18:07:52 -0500

    Adding value by default a JwtConfig.

--- src/main/java/com/kerno/utils/jwt/JwtConfig.java
+++ src/main/java/com/kerno/utils/jwt/JwtConfig.java
@@ -1,12 +1,20 @@
 package com.kerno.utils.jwt;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Data
 public class JwtConfig {
+    @Value("${kerno.jwt.uri:/kerno-security/auth/**}")
     private String url;
+    @Value("${kerno.jwt.header:Authorization}")
     private String header;
+    @Value("${kerno.jwt.prefix:Bearer }")
     private String prefix;
+    @Value("${kerno.jwt.expiration:86400000}")
     private long expiration;
+    @Value("${kerno.jwt.secret:kerno-secret}")
     private String secret;
+
 }
