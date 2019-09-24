6ada381b3d1e0c024c9fe60ab7aa5f23124e4b8f
Jorge Luis Malla <jomasloja@gmail.com>
2019-09-24 00:04:32 -0500

    Adding constructor in KernoPageable.

--- src/main/java/com/kerno/utils/cqrs/KernoPageable.java
+++ src/main/java/com/kerno/utils/cqrs/KernoPageable.java
@@ -9,4 +9,18 @@ public class KernoPageable {
     private int pageNumber;
     private int pageSize;
     private String sort;
+
+    public KernoPageable() {
+    }
+
+    public KernoPageable(int pageNumber, int pageSize) {
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+    }
+
+    public KernoPageable(int pageNumber, int pageSize, String sort) {
+        this.pageNumber = pageNumber;
+        this.pageSize = pageSize;
+        this.sort = sort;
+    }
 }
