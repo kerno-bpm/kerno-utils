3f3e6f8ebf586f5f2802a03f715183574967f015
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2020-04-10 23:17:20 -0500

    Adding UUID in DTO.

--- src/main/java/com/kerno/utils/dto/AbstractExternalIdDto.java
+++ src/main/java/com/kerno/utils/dto/AbstractExternalIdDto.java
@@ -2,7 +2,9 @@ package com.kerno.utils.dto;
 
 import lombok.Data;
 
+import java.util.UUID;
+
 @Data
 public class AbstractExternalIdDto extends AbstractDto {
-    private String externalId;
+    private UUID externalId;
 }
