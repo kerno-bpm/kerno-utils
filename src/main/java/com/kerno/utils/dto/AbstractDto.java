80e42f28f1c0176123b54c2a0760406f259f73fa
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2020-05-11 00:04:57 -0500

    Adding dates in abstractdto.

--- src/main/java/com/kerno/utils/dto/AbstractDto.java
+++ src/main/java/com/kerno/utils/dto/AbstractDto.java
@@ -3,11 +3,14 @@ package com.kerno.utils.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 
 @Data
 public class AbstractDto implements Serializable {
     private Long id;
     private boolean active;
+    private Timestamp created;
+    private Timestamp updated;
 
     public AbstractDto() {
         active = true;
