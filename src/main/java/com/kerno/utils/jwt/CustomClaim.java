c838d172e410ac24a0a3f048d005dbfd512727a6
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2020-05-30 23:59:14 -0500

    Adding authorities

--- src/main/java/com/kerno/utils/jwt/CustomClaim.java
+++ src/main/java/com/kerno/utils/jwt/CustomClaim.java
@@ -2,8 +2,12 @@ package com.kerno.utils.jwt;
 
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 public class CustomClaim {
     private String accountId;
     private boolean isSuperUser;
+    private List<String> authorities = new ArrayList<>();
 }
