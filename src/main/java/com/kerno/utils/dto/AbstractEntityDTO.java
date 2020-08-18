740584626175df75960b8cbb3a0f17d453139025
Jorge Luis Malla Sanchez <jomasloja@gmail.com>
2020-08-18 10:50:43 -0500

    formatting the date.

--- src/main/java/com/kerno/utils/dto/AbstractEntityDTO.java
+++ src/main/java/com/kerno/utils/dto/AbstractEntityDTO.java
@@ -1,5 +1,6 @@
 package com.kerno.utils.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,7 +8,9 @@ import java.time.LocalDateTime;
 @Data
 public class AbstractEntityDTO extends AbstractIdDTO {
     private boolean active;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime created;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime updated;
 
     public AbstractEntityDTO() {
